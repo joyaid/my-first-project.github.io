813e715178e3e119b03b096d725709982cdcaffb
joyaid <hitopik54@gmail.com>
2026-02-24 19:54:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 30 },
+    "color": { "value": "#ffffff" },
+    "opacity": { "value": 0.15 },
+    "size": { "value": 2 },
+    "line_linked": { "enable": true, "distance": 200, "color": "#ffffff", "opacity": 0.05, "width": 1 },
+    "move": { "enable": true, "speed": 0.5 }
+  }
+});
